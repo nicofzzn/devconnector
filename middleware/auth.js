57ec400c19965f0aeb31608a73b444a59c12a400
nicofzzn <nicofaza975@gmail.com>
2020-07-12 03:23:58 +0700

    protected route for dashboard

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,8 +6,7 @@ module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
 
   // check if there is no token
-  if (!token)
-    res.status(401).json({ msg: 'No token, authorization denied' });
+  if (!token) res.status(401).json({ msg: 'No token, authorization denied' });
 
   //verify token
   try {
